142051c3f2152d6a686547b04031b4876ca6aefd
Jean Gabriel <jeangabriel180@hotmail.com>
2022-03-09 23:01:49 -0300

    criando projeto

---
+++ src/entities/BrazilInterestService.java
@@ -0,0 +1,16 @@
+package entities;
+
+import java.security.InvalidParameterException;
+
+public class BrazilInterestService implements InterestService {
+    private double interestRate;
+
+    public BrazilInterestService(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    @Override
+    public double getInterestRate() {
+        return interestRate;
+    }
+}
